9df984f10e7aec69002e0eb0bcd688832da9ec56
sathishdevops38 <sathishdevops38@gmail.com>
2026-02-09 15:56:11 +0530

    cmd

--- 02_Docker_Commands/02-01-ec2-docker-setup/main.tf
+++ 02_Docker_Commands/02-01-ec2-docker-setup/main.tf
@@ -3,8 +3,6 @@ resource "aws_instance" "docker"{
     instance_type = var.instance_type
     subnet_id = local.subnet_id
     security_groups = [local.sg_id]
-    root_volume_size = 50
-    root_volume_type = "gp3"
     user_data = file("docker.sh")
     root_block_device {
     volume_size = 50
